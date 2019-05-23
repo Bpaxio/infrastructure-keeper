71f3fa231cd1e9d53a539db50b06994093dfaf61
Bpaxio <bbpax@yandex.ru>
2019-05-23 16:34:46 +0300

    added eureka+zuul-proxy+config-server

---
+++ gateway/src/main/java/ru/bbpax/keeper/gateway/config/SwaggerDocController.java
@@ -0,0 +1,30 @@
+package ru.bbpax.keeper.gateway.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Primary
+@EnableAutoConfiguration
+public class SwaggerDocController implements SwaggerResourcesProvider {
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        resources.add(swaggerResource("files-keeper", "/api/v2/files/api-docs", "2.0"));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+}
